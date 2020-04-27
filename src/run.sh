af13e98e38c0843cb726ac5a456aa5a21e8128a9
AlexQ333 <36330011+AlexQ333@users.noreply.github.com>
2020-04-27 22:56:38 +1000

    Updated run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,7 +1,7 @@
 echo "Welcome to **TERMINAL NOTES**"
 echo "Lets begin the install now"
-git clone "https://github.com/https://github.com/alex_q/asdf.git"
-cd terminal_notes/src
+git clone "https://github.com/AlexQ333/Notes_app.git"
+cd Notes_app/src
 echo "All required gems are being installed right now"
 bundle install
 echo "All gems have been installed, running application for the first time now"
