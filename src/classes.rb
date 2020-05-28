bc8f318e9c5630ab5c4ffcd6f1fb36931556cc7b
Alex Qiang <alex.qiang36@gmail.com>
2020-05-29 00:17:14 +1000

    Alter noteboard_menu method

--- src/classes.rb
+++ src/classes.rb
@@ -4,17 +4,17 @@ require "tty-table"
 require "csv"
 
 # Display a start menu to user
-# class StartMenu 
-#     def initialize
-#         @start_prompt = TTY::Prompt.new
-#         @start_prompt.select("Create new noteboard or load existing one?") do |menu|
-#             menu.choice "New noteboard", -> {new_noteboard}
-#             menu.choice "Load existing noteboard", -> {noteboard_menu}
-#             menu.choice "Exit", -> {system "clear"}
-#         end
-#     end  
+class StartMenu 
+    def initialize
+        @start_prompt = TTY::Prompt.new
+        @start_prompt.select("Create new noteboard or load existing one?") do |menu|
+            menu.choice "New noteboard", -> {new_noteboard}
+            menu.choice "Load existing noteboard", -> {noteboard_menu}
+            menu.choice "Exit", -> {system "clear"}
+        end
+    end  
     
-# end
+end
 
 
 
@@ -28,19 +28,22 @@ class Noteboard
     
     
     
-    # Reads and displays CSV fil data into a table
+    # Adds note into current notebaord
     def noteboard_add(note)
         file = CSV.open(@file, "a+") do |csv|
             csv << [note]
         end
+        file
 
     end
 
+    # Deletes a line of notes from current noteboard
     def noteboard_delete(line_number)
         
 
     end
 
+    # Displays all notes to user
     def display_noteboard(file)
         puts "Your notes are"
         puts file_data = File.read(file)
@@ -55,6 +58,20 @@ end
 
 #functions
 
+# Asks user for new noteboard name
+def new_noteboard
+    puts "Enter a name for this new noteboard."
+    file_name = gets.chomp
+    
+end
+
+# Asks and deletes a noteboard file that user requests
+def delete_noteboard
+    puts "Enter just the name of noteboard to be deleted:"
+    delete_file = gets.chomp
+    TTY::File.remove_file "#{delete_file}.csv"
+end
+
 #Finds any existing noteboard files and displays a selection menu
 def noteboard_menu
     @file_arr = Dir.glob("*csv")
@@ -63,25 +80,12 @@ def noteboard_menu
         for file in @file_arr do
         menu.choice "#{file}", -> {Noteboard.display_noteboard(file)}
         end
-        menu.choice "Delete a noteboard"
+        menu.choice "Delete a noteboard", -> {delete_noteboard}
         menu.choice "Back", -> {StartMenu.new}
     end
 end
 
-
-def new_noteboard
-    puts "Enter a name for this new noteboard."
-    file_name = gets.chomp
-    
-end
-
-def delete_noteboard
-    puts "Enter name of noteboard to be deleted:"
-    delete_file = gets.chomp
-    TTY::File.remove_file "#{delete_file}.csv"
-end
-
-
+# Asks user for note to be added
 def add_note
     puts "Enter your note: "
     note = gets.chomp
@@ -91,17 +95,18 @@ end
 def delete_note
     puts "Which Line do you want to delete (Enter Line number):"
     # error handling here, numbers only
-    line_number = get.chomp.to_i
+    line_number = gets.chomp.to_i
 
 end
 
+# Options menu that methods should loop back into
 def options_menu
     @start_prompt = TTY::Prompt.new
         @start_prompt.select("What do you want to do now") do |menu|
             menu.choice "Show my notes", -> {Noteboard.display_noteboard}
             menu.choice "Add new note", -> {Noteboard.noteboard_add(add_note)}
             menu.choice "Delete note", -> {Noteboard.noteboard_delete(delete_note)}
-            menu.choice "Exit", -> {Startmenu.new}
+            menu.choice "Back", -> {StartMenu.new}
         end
 end
 
@@ -112,7 +117,7 @@ noteboard = Noteboard.new(new_noteboard)
 
 noteboard.noteboard_add(add_note)
 
-noteboard.display_noteboard
+# noteboard.display_noteboard(file)
 
 options_menu
 
