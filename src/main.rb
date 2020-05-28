1b38db2c74450fcdd1356dd250f58586da9184b6
Alex Qiang <alex.qiang36@gmail.com>
2020-05-28 21:25:31 +1000

    Add ARGV components

--- src/main.rb
+++ src/main.rb
@@ -5,6 +5,18 @@ require "tty-file"
 require "tty-table"
 require "colorize"
 
+Args = ARGV
+if args[0] == "new"
+    $prompt.keypress("Press enter to add a new note", keys: [:return])
+    add_note
+elsif args[0] == "display"
+    noteboard_menu
+
+end
+
 title = Artii::Base.new
 puts title.asciify("Terminal Notes").colorize(:light_blue)
 StartMenu.new
+
+
+
