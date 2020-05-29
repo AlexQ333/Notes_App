a057dc075894ee1ce9f0d09b78d892430f1198c3
Alex Qiang <alex.qiang36@gmail.com>
2020-05-29 16:52:37 +1000

    Clean up writing

--- src/help.rb
+++ src/help.rb
@@ -1,27 +1,14 @@
 def help_info
     puts "Command line arguments available in Terminal Notes are:"
     
-    puts "(ruby main.rb h) opens the help file which include arguments available and installation guide"
+    puts "h:       opens the help file which include arguments available and installation guide"
    
-    puts "(ruby main.rb new) skips the start menu and allow user to create new noteboard file and enter first note "
+    puts "new:     skips the start menu and allow user to create new noteboard file and enter first note "
     
-    puts "(ruby main.rb display) opens men containing existing noteboard files"
+    puts "display: opens men containing existing noteboard files"
     
+    puts "To install the program, execute run.sh"
     
-    
-    
-    puts "Installation Guide"
 
-    puts "First, download and install Ruby on your computer:"    
-    
-    puts "sudo apt-get install ruby"
-    
-    puts "Install Bundler Gem:"
-    
-    puts "gem install bundler"
-    
-    puts "Next step is to run run.sh"
-    
-      
     
 end
