ee602e8a73be9f76a3cf3072c5191d6d1ae31b24
Alex Qiang <alex.qiang36@gmail.com>
2020-05-29 14:57:10 +1000

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,8 +1,15 @@
 echo "Welcome to **TERMINAL NOTES**"
+
 echo "Lets begin the install now"
+
 git clone "https://github.com/AlexQ6/Notes_app.git"
-cd Notes_app/src
+
+cd src
+
 echo "All required gems are being installed right now"
+
 bundle install
+
 echo "All gems have been installed, running application for the first time now"
+
 ruby main.rb
