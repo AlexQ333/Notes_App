682e26fa57edbaa8da9e726be84f6d576b6a0d44
Alex Qiang <alex.qiang36@gmail.com>
2020-05-29 16:09:37 +1000

    Fix args issues

--- src/main.rb
+++ src/main.rb
@@ -8,7 +8,20 @@ require "colorize"
 
 
 args = ARGV
-if args[0].downcase == "new"
+
+
+if args.empty?
+    title = Artii::Base.new
+    puts title.asciify("Terminal Notes").colorize(:light_blue)
+    #Control flow
+
+    StartMenu.new
+    noteboard = Noteboard.new(new_noteboard)
+    noteboard.noteboard_add(add_note)
+    noteboard.display_noteboard(file)
+    options_menu(noteboard)
+
+elsif args[0].downcase == "new"
     noteboard = Noteboard.new(new_noteboard)
     noteboard.noteboard_add(add_note)
 
@@ -18,25 +31,14 @@ elsif args[0].downcase == "display"
 elsif args[0].downcase == "h"
     help_info
 
+
 end
 
 
 
 
-title = Artii::Base.new
-puts title.asciify("Terminal Notes").colorize(:light_blue)
 
-# #Control flow
 
-StartMenu.new
-
-noteboard = Noteboard.new(new_noteboard)
-
-noteboard.noteboard_add(add_note)
-
-noteboard.display_noteboard(file)
-
-options_menu(noteboard)
 
 
 
