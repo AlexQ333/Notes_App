f5fe955992c48e494379b12a5f4ad57116ac7038
Alex Qiang <alex.qiang36@gmail.com>
2020-05-29 15:26:52 +1000

    Add help file

---
+++ src/help.rb
@@ -0,0 +1,27 @@
+def help_info
+    puts "Command line arguments available in Terminal Notes are:"
+    
+    puts "(ruby main.rb h) opens the help file which include arguments available and installation guide"
+   
+    puts "(ruby main.rb new) skips the start menu and allow user to create new noteboard file and enter first note "
+    
+    puts "(ruby main.rb display) opens men containing existing noteboard files"
+    
+    
+    
+    
+    puts "Installation Guide"
+
+    puts "First, download and install Ruby on your computer:"    
+    
+    puts "sudo apt-get install ruby"
+    
+    puts "Install Bundler Gem:"
+    
+    puts "gem install bundler"
+    
+    puts "Next step is to run run.sh"
+    
+      
+    
+end
