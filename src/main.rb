0313fc16a3fcc493fe6b5ccf18817dfc9f6eb50d
Alex Qiang <alex.qiang36@gmail.com>
2020-05-29 14:35:30 +1000

    Move control flow into main.rb

--- src/main.rb
+++ src/main.rb
@@ -1,14 +1,16 @@
-require_relative "./classes.rb"
+require_relative "classes.rb"
 require "artii"
 require "tty-prompt"
 require "tty-file"
 require "tty-table"
 require "colorize"
 
-Args = ARGV
+args = ARGV
 if args[0] == "new"
-    $prompt.keypress("Press enter to add a new note", keys: [:return])
-    add_note
+    # $prompt.keypress("Press enter to add a new note", keys: [:return])
+    noteboard = Noteboard.new(new_noteboard)
+    noteboard.noteboard_add(add_note)
+    
 elsif args[0] == "display"
     noteboard_menu
 
@@ -16,7 +18,18 @@ end
 
 title = Artii::Base.new
 puts title.asciify("Terminal Notes").colorize(:light_blue)
+
+# #Control flow
+
 StartMenu.new
+
+# noteboard = Noteboard.new(new_noteboard)
+
+# noteboard.noteboard_add(add_note)
+
+# # noteboard.display_noteboard(file)
+
+# options_menu
 
 
 
